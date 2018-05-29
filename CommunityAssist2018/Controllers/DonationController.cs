51c2e674a855d4e9028a59f7347e5569cc3e5256
Solomon Askanaw <solomon.ab1988@gmail.com>
2018-05-29 11:40:52 -0700

    Donation Homework

---
+++ CommunityAssist2018/Controllers/DonationController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2018.Models;
+
+namespace CommunityAssist2018.Controllers
+
+{
+    public class DonationsController : Controller
+
+    {
+
+        // GET: Donations
+
+        public ActionResult Index()
+
+        {
+
+            if (Session["PersonKey"] == null)
+
+            {
+
+                return RedirectToAction("Index", "Login");
+
+            }
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index([Bind(Include = "DonationKey, DonationDate, DonationAmount, DonationConfirmation")]Donation d)
+
+
+
+        {
+
+            d.DonationConfirmationCode = Guid.NewGuid();
+
+            d.PersonKey = (int)Session["PersonKey"];
+
+            d.DonationDate = DateTime.Now;
+
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+            db.Donations.Add(d);
+
+            db.SaveChanges();
+
+            Message m = new Message("There is a new donation.");
+
+            return View("Result", m);
+
+        }
+
+        public ActionResult Result(Message msg)
+
+        {
+
+            return View(msg);
+
+        }
+
+
+
+    }
+
+}
